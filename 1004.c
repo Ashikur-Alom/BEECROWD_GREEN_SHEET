9f1f604b183aaebd987198b304ae8b3a21ddf7a7
Md Ashikur Alom Sarkar <ashikuralom5@gmail.com>
2025-10-17 04:01:24 +0600

    Update 1004.c

--- 1004.c
+++ 1004.c
@@ -4,11 +4,8 @@ int main() {
     int a,b,prod;
     scanf("%d %d", &a, &b);
     
-    prod = a * b;
-    
-    if(prod >= 0 || prod <= 0) {
-        printf("PROD = %d\n", prod);
-    }
+    prod = a * b; 
+    printf("PROD = %d\n", prod);
     
     return 0;
 }
