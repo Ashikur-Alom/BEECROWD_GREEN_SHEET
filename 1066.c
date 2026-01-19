791c9fdd172724547758d635bd4d906dc8bdd234
Md Ashikur Alom Sarkar <ashikuralom5@gmail.com>
2026-01-19 22:39:11 +0600

    Create 1066.c

---
+++ 1066.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+int n,even=0,odd=0,posi=0,nega=0;
+for(int i=1; i<=5; i++) {
+    scanf("%d", &n);
+
+    if(n>0) {
+        posi++;
+    }else if(n<0) {
+        nega++;
+    }
+
+    if(n%2==0) {
+        even++;
+    }else {
+        odd++;
+    }
+}
+
+printf("%d valor(es) par(es)\n", even);
+printf("%d valor(es) impar(es)\n", odd);
+printf("%d valor(es) positivo(s)\n", posi);
+printf("%d valor(es) negativo(s)\n", nega);
+
+
+    return 0;
+}
