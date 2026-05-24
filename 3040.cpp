b838b8a39baf6c088e985b7e882f32d9afce7295
Md Ashikur Alom Sarkar <ashikuralom5@gmail.com>
2026-05-25 00:15:17 +0600

    Create 3040.cpp

---
+++ 3040.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int n;
+cin >> n;
+
+while(n--) {
+    int h,d,g;
+    cin >> h >> d >> g;
+    
+    if((h>=200 && h<=300) && d>=50 && g>=150) cout << "Sim\n";
+    else cout << "Nao\n";
+}
+    
+    return 0;
+}
